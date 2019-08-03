db8cdd9442d570ac01474e9454721cd612db9979
Anderson Schmidt <schmidtanderson@hotmail.com>
2019-08-03 19:09:06 -0300

    Removing query from Pet.find

--- api/app.js
+++ api/app.js
@@ -29,7 +29,7 @@ const storage = multer.diskStorage({
 });
 
 app.get("/", (req, res) => {
-  Pet.find({active: true}).then(pets => {
+  Pet.find().then(pets => {
     res.status(200).json(pets);
   }).catch(err => {
     console.log(err);
