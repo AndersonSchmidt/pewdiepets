621cc1d0d6b37f7d9c105cecb2a851a716796f80
Anderson Schmidt <schmidtanderson@hotmail.com>
2019-08-18 17:11:08 -0300

    Unsubscribing from the Subject onDestroy

--- client/src/app/pets/pets.component.ts
+++ client/src/app/pets/pets.component.ts
@@ -1,14 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { PetService } from '../pet.service';
 import { Pet } from '../pet.model';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-pets',
   templateUrl: './pets.component.html',
   styleUrls: ['./pets.component.css']
 })
-export class PetsComponent implements OnInit {
+export class PetsComponent implements OnInit, OnDestroy {
 
+  searchSub: Subscription;
   pets: Pet[];
   petsCopy: Pet[];
   constructor(private petService: PetService) { }
@@ -19,11 +21,15 @@ export class PetsComponent implements OnInit {
       this.petsCopy = pets;
     });
 
-    this.petService.search.subscribe((name) => {
+    this.searchSub = this.petService.search.subscribe((name) => {
       this.pets = this.petsCopy.filter((pet) => {
         return pet.name.match(new RegExp(name, 'i'));
       });
     });
+  }
+
+  ngOnDestroy() {
+    this.searchSub.unsubscribe();
   }
 
 }
